563eef69e1310ad19618d1d17b362849e61d67e6
OnStarPrograms <aidenthomas711@gmail.com>
2024-10-06 19:41:55 -0400

    fixed the bot, and changed file structure

--- player.py
+++ player.py
@@ -2,36 +2,36 @@ import random
 
 class player:
     def __init__(self, cards: list[int]):
-        self.__card = cards
+        self.card = cards
         self.score = 0
-        self.__cardInHand = cards[random.randint(0, len(cards)-1)]
+        self.cardInHand = cards[random.randint(0, len(cards)-1)]
     def placeCardInHand(self, pastChoices):
         # pdb.set_trace()
-        choice = self.__card[random.randint(0, len(self.__card)-1)]
-        self.__cardInHand = choice
+        choice = self.card[random.randint(0, len(self.card)-1)]
+        self.cardInHand = choice
         return choice 
     
     def PlayerResponse(self, card: int):
-        for i in range(len(self.__card)):
-            if (card == self.__card[i]):
-                self.__card.remove(self.__card[i])
+        for i in range(len(self.card)):
+            if (card == self.card[i]):
+                self.card.remove(self.card[i])
                 return True
         return False
 
     def GoFish(self, card: int):
-        self.__card.append(card)
+        self.card.append(card)
         
     def AskPlayer(self) -> int:
-        return self.__cardInHand
+        return self.cardInHand
 
     def GetScore(self):
         return self.score
     
     def PlayerReceived(self):
             self.score+=1
-            for i in range(0, len(self.__card)):
-                if (self.__cardInHand == self.__card[i]):
-                    self.__card.remove(self.__card[i])
+            for i in range(0, len(self.card)):
+                if (self.cardInHand == self.card[i]):
+                    self.card.remove(self.card[i])
                     break
             
         
